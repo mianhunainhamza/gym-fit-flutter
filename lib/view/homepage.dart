e43bd09ff6353fa6886833c32f9b246b7430b447
Abdullah Ahmad <abdullahahmad319.319@gmail.com>
2024-02-24 13:27:53 +0500

    homepage

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,10 +1,56 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(28),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Hello dude",
+                          style: GoogleFonts.poppins(fontSize: 22)),
+                      const SizedBox(height: 5),
+                      Text(
+                        "Let's get workout.",
+                        style: GoogleFonts.poppins(
+                          fontSize: 22,
+                          color: Colors.amber,
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 55,
+                    width: 55,
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20000),
+                      ),
+                      child: Image.asset(
+                        "assets/images/dp.jpg",
+                        filterQuality: FilterQuality.high,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
