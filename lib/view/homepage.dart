85b1f9a65624e1b956ffa750f9a5fc4b18ed8979
Abdullah Ahmad <abdullahahmad319.319@gmail.com>
2024-02-24 17:48:54 +0500

    homepage_ui

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Future<void> refresh() {
+      return Future.delayed(const Duration(seconds: 2));
+    }
+
     return Scaffold(
         body: RefreshIndicator(
           displacement: Get.height * 0.1,
