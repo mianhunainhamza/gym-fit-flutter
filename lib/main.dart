258a86c8d20766cb0bc89a905f0ab77245084cd7
Abdullah Ahmad <abdullahahmad319.319@gmail.com>
2024-02-25 00:31:20 +0500

    search

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:softec_app_dev/view/Home/bottom_navigation.dart';
 import 'package:softec_app_dev/view/Home/homepage.dart';
+import 'package:softec_app_dev/view/Home/search.dart';
 import 'package:softec_app_dev/view/sign_up_page.dart';
 import 'firebase_options.dart';
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home:  BottomNavigation(),
+      home: Search(),
     );
   }
 }
--- lib/view/Home/search.dart
+++ lib/view/Home/search.dart
@@ -1,13 +1,142 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:line_icons/line_icons.dart';
 
 class Search extends StatelessWidget {
   const Search({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Search Screen')),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 36),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: Get.width * 0.12,
+                      width: Get.width * 0.12,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20000),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/dp.jpg"),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: Get.width * 0.02),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Veronica",
+                              style: GoogleFonts.poppins(
+                                fontSize: Get.width * 0.04,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              "@veronica",
+                              style: GoogleFonts.poppins(
+                                fontSize: Get.width * 0.035,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: Get.height * 0.01),
+                        Text(
+                          "My house is complete with stylish furniture \nthat adds a touch of comfort.",
+                          style: GoogleFonts.poppins(
+                            fontSize: Get.width * 0.036,
+                          ),
+                        ),
+                        SizedBox(height: Get.height * 0.02),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          height: Get.height * 0.25,
+                          width: Get.width * 0.7,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                            image: DecorationImage(
+                              filterQuality: FilterQuality.high,
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/furniture.jpg"),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: Get.height * 0.02),
+                        Text(
+                          "8m",
+                          style: GoogleFonts.poppins(
+                            color: Colors.grey.withOpacity(0.8),
+                            fontSize: Get.width * 0.04,
+                          ),
+                        ),
+                        SizedBox(height: Get.height * 0.015),
+                        Container(
+                          width: Get.width * 0.7,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  const Icon(LineIcons.thumbsUp),
+                                  SizedBox(width: Get.width * 0.02),
+                                  Text(
+                                    "23",
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.grey.withOpacity(0.7),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(Icons.chat),
+                                  SizedBox(width: Get.width * 0.02),
+                                  Text(
+                                    "1",
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.grey.withOpacity(0.7)),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(LineIcons.thumbsUp),
+                                  SizedBox(width: Get.width * 0.02),
+                                  Text(
+                                    "16",
+                                    style: GoogleFonts.poppins(
+                                        color: Colors.grey.withOpacity(0.7)),
+                                  ),
+                                ],
+                              ),
+                              const Icon(LineIcons.share),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
