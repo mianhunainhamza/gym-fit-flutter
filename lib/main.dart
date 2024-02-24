d930bf9701a3d26f5373237518bb6434117820a1
MIAN HUNAIN <mianhunainhamza@gmail.com>
2024-02-24 13:45:18 +0500

    add onboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:softec_app_dev/firebase_options.dart';
-import 'package:softec_app_dev/view/homepage.dart';
 import 'package:softec_app_dev/view/onboard_page.dart';
 
-void main() async {
+import 'firebase_options.dart';
+
+void main() async  {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const OnBoardPage(),
     );
   }
 }
--- lib/view/onboard_page.dart
+++ lib/view/onboard_page.dart
@@ -5,10 +5,58 @@ class OnBoardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        child: Image.asset('assets/images/girl.png'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+      ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              //
+              const SizedBox(
+                height: 30,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 40),
+                child: SizedBox(
+                  width: size.width,
+                  height: 200,
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontSize: size.height * 0.055,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromRGBO(253, 220, 138, 1), // Set default color
+                      ),
+                      children: [
+                        const TextSpan(
+                          text: 'YOU READY TO ',
+                        ),
+                        TextSpan(
+                          text: 'GET FIT',
+                          style: TextStyle(color: Colors.black.withOpacity(.7)), // Set black color for "GET FIT"
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+          //image
+          Container(
+            decoration: const BoxDecoration(
+            ),
+              width: size.width,
+              height: size.height-300,
+              child: Image.asset('assets/images/girl.png',fit: BoxFit.fitHeight,)),
+        ],
       ),
     );
   }
