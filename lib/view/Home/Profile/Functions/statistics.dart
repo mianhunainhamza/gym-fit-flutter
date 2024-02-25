b09cce39d91cc30cc7099b93e4e7bd533fcdbc44
MIAN HUNAIN <mianhunainhamza@gmail.com>
2024-02-25 06:52:47 +0500

    add live track

--- lib/view/Home/Profile/Functions/statistics.dart
+++ lib/view/Home/Profile/Functions/statistics.dart
@@ -1,19 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pedometer/pedometer.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Statistics extends StatefulWidget {
-  const Statistics({super.key});
-
+  const Statistics({Key? key}) : super(key: key);
 
   @override
-  _StatisticsState createState() => _StatisticsState();
+  StatisticsState createState() => StatisticsState();
 }
 
-class _StatisticsState extends State<Statistics> {
+class StatisticsState extends State<Statistics> {
   late Stream<StepCount> _stepCountStream;
   int _steps = 0;
 
@@ -40,39 +38,72 @@ class _StatisticsState extends State<Statistics> {
     _stepCountStream = Pedometer.stepCountStream;
 
     // Listen to streams and handle errors
-    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+    _stepCountStream.listen(onStepCount, onError: onStepCountError);
   }
 
   @override
   Widget build(BuildContext context) {
+    List<_SalesData> data = [
+      _SalesData('Jan', 35),
+      _SalesData('Feb', 28),
+      _SalesData('Mar', 34),
+      _SalesData('Apr', 32),
+      _SalesData('May', 40)
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Statistics',style: GoogleFonts.poppins(fontWeight: FontWeight.bold),),
-        centerTitle: true,
         automaticallyImplyLeading: false,
-        leading: IconButton(
-          onPressed: () {
+        leading: GestureDetector(
+          onTap: () {
             Get.back();
           },
-          icon: const Icon(Icons.arrow_back_ios_new),
+          child: const Icon(Icons.arrow_back_ios_new),
+        ),
+        centerTitle: true,
+        title: Text(
+          'Stats',
+          style: GoogleFonts.poppins(
+            fontSize: Get.height * 0.03,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Steps Count:',
-              style: TextStyle(fontSize: 20),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Steps Count:',
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            '$_steps',
+            style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: Get.height * .6,
+            width: Get.width,
+            child: SfCircularChart(
+              series: <CircularSeries<_SalesData, String>>[
+                RadialBarSeries<_SalesData, String>(
+                  radius: '50%',
+                  dataSource: data,
+                  xValueMapper: (_SalesData sales, _) => sales.year,
+                  yValueMapper: (_SalesData sales, _) => sales.sales,
+                  dataLabelSettings: const DataLabelSettings(isVisible: true),
+                ),
+              ],
             ),
-            Text(
-              '$_steps',
-              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
 
+class _SalesData {
+  _SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
+}
